141a0b439ed9e3ef3f6a98a1be042a17de72b1cc
junote <junote@outlook.com>
2021-04-04 23:53:17 +0800

    add simple structure

---
+++ base/vhdl/sillyfunction.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity sillyfunction is
+  port (
+    a, b, c : in std_logic;
+    y       : out std_logic;
+
+  );
+end entity sillyfunction;
+
+architecture rtl of sillyfunction is
+
+begin
+
+  y <= ((not a) and (not b) and (not c))or
+    (a and (not b) and (not c))or
+    (a and (not b) and c);
+    
+end architecture;
